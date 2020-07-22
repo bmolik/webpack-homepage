91b1a0f301eee2f92f4d34f23bc2f5a046c749e9
Bartosz Molik <brth182@gmail.com>
2020-07-22 23:06:21 +0200

    initial commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import '../scss/main.scss';
+import moment from 'moment';
 
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
@@ -7,3 +8,31 @@ import '../scss/main.scss';
 /* place your code below */
 
 console.log('HELLO 🚀')
+const name = "Kotek";
+const age = 1.5;
+
+console.log(name);
+console.log(age);
+/* const message = document.querySelector('.hidden-one__header');
+message.innerHTML = "Łajby są super!!!";
+*/
+
+function greet(age, name) {
+    console.log(`Kotek ${name} tak. I ma ${age} latek.`);
+
+}
+
+const newGreet = (age, name) => {
+    console.log(`Kotek ${name} tak. I ma ${age} latek.`);
+}
+
+greet(age, name);
+newGreet(age, name);
+
+const hamburger = document.querySelector('.hamburger--js');
+hamburger.addEventListener('click', () => {
+    const nav = document.querySelector('.navigation--js');
+    nav.classList.toggle('navigation--open');
+})
+
+console.log(moment().startOf('day').fromNow());
